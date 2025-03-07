183da7af5564959ccc38317311cc3757477a4eea
Deen <01kiprotich@gmail.com>
2025-03-08 00:03:16 +0300

    commit commign

---
+++ README.md
@@ -0,0 +1,35 @@
+Bitcoin Nairobi project This is a shortcut 
+<<<<<<< HEAD
+
+img="![alt text](image.png)";
+
+=======
+>>>>>>> b2e3e48c9a41d7cbb89725c059fedc618391d26b
+
+img="![alt text](image.png)";
+- Vite
+- React.js
+- Tailwind CSS
+
+
+
+```bash
+git clone [https://github.com/DeenRamah/btcnairobi]
+cd brainwave
+```
+
+**Installation**
+
+Install the project dependencies using npm:
+
+```bash
+npm install
+```
+
+**Running the Project**
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:5173](http://localhost:5173) in your browser to view the project.
---
+++ src/constants/index.js
@@ -0,0 +1,371 @@
+import {
+  benefitIcon1,
+  benefitIcon2,
+  benefitIcon3,
+  benefitIcon4,
+  benefitImage2,
+  chromecast,
+  disc02,
+  discord,
+  discordBlack,
+  facebook,
+  linkedin,
+  figma,
+  file02,
+  framer,
+  homeSmile,
+  instagram,
+  notification2,
+  notification3,
+  felogo,
+  mylogo,
+  hislogo,
+  dlogo,
+  notification4,
+  notion,
+  photoshop,
+  plusSquare,
+  protopie,
+  raindrop,
+  recording01,
+  recording03,
+  roadmap1,
+  roadmap2,
+  roadmap3,
+  roadmap4,
+  roadmap5,
+  roadmap6,
+  searchMd,
+  slack,
+  sliders04,
+  telegram,
+  twitter,
+  yourlogo,
+} from "../assets";
+
+export const navigation = [
+  {
+    id: "0",
+    title: "Home",
+    url: "#features",
+  },
+  {
+    id: "1",
+    title: "Programs",
+    url: "#pricing",
+  },
+  {
+    id: "2",
+    title: "About Us",
+    url: "#how-to-use",
+  },
+  {
+    id: "3",
+    title: "Roadmap",
+    url: "#roadmap",
+  },
+  {
+    id: "4",
+    title: "Join Us",
+    url: "https://web3-five-wine.vercel.app",
+    onlyMobile: true,
+  },
+  {
+    id: "5",
+    title: "Support Us",
+    url: "https://geyser.fund/project/btcnairobi?mtm_campaign=project-share&mtm_keyword=btcnairobi&mtm_source=user&mtm_medium=geyser&mtm_content=contribution-summary",
+    onlyMobile: true,
+  },
+];
+
+export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
+
+export const notificationImages = [notification4, notification3, notification2];
+
+export const companyLogos = [yourlogo,  mylogo, dlogo, felogo, hislogo];
+
+export const brainwaveServices = [
+  "Education Programs",
+  "Community Events",
+  "Seamless Services",
+];
+
+export const brainwaveServicesIcons = [
+  recording03,
+  recording01,
+  disc02,
+  chromecast,
+  sliders04,
+];
+
+
+export const roadmap = [
+  {
+    id: "0",
+    title: "Noones Integration",
+    text: "Integrate with Noones to offer a peer-to-peer Bitcoin marketplace, enhancing user buying and selling options.",
+    date: new Date().toLocaleDateString(),
+    status: "done",
+    imageUrl: roadmap1,
+    colorful: true,
+  },
+  {
+    id: "1",
+    title: "Paxful Integration",
+    text: "Enable users to buy and sell Bitcoin through Paxful, providing diverse payment methods and global reach.",
+    date: new Date().toLocaleDateString(),
+    status: "progress",
+    imageUrl: roadmap2,
+  },
+  {
+    id: "2",
+    title: "Binance API Access",
+    text: "Allow users to trade Bitcoin on Binance, accessing advanced trading features and liquidity.",
+    date: new Date().toLocaleDateString(),
+    status: "done",
+    imageUrl: roadmap3,
+  },
+  {
+    id: "3",
+    title: "AstroPay Support",
+    text: "Integrate AstroPay for easy Bitcoin purchases using local currencies, improving accessibility.",
+    date: new Date().toLocaleDateString(),
+    status: "progress",
+    imageUrl: roadmap4,
+  },
+  {
+    id: "4",
+    title: "Bitcoin.com Integration",
+    text: "Provide direct access to Bitcoin.com resources and services, enhancing user knowledge and transaction capabilities.",
+    date: new Date().toLocaleDateString(),
+    status: "planned",
+    imageUrl: roadmap5,
+  },
+  {
+    id: "5",
+    title: "LunoPay Integration",
+    text: "Integrate with LunoPay to allow seamless Bitcoin payments and transfers, expanding user payment options.",
+    date: new Date().toLocaleDateString(),
+    status: "planned",
+    imageUrl: roadmap6,
+  },
+];
+
+
+export const collabText =
+  "With smart automation and top-notch security, it's the perfect solution for teams looking to work smarter.";
+
+export const collo =
+"Bitcoin Nairobi integrates education and application, empowering confident cryptocurrency navigation.";
+export const collora =
+"Bitcoin Nairobi simplifies learning with smart automation, making Bitcoin accessible and user-friendly";
+
+export const collops =
+"Bitcoin Nairobi ensures safe transactions and secure wallet management for peace of mind";
+
+
+export const collabContent = [
+  {
+    id: "0",
+    title: "Seamless Integration",
+    text: collo,
+  },
+  {
+    id: "1",
+    title: "Smart Automation",
+    text: collora,
+  },
+  {
+    id: "2",
+    title: "Top-notch Security",
+    text: collops,
+  },
+];
+
+export const collabApps = [
+  {
+    id: "0",
+    title: "Figma",
+    icon: figma,
+    width: 26,
+    height: 36,
+  },
+  {
+    id: "1",
+    title: "Notion",
+    icon: notion,
+    width: 34,
+    height: 36,
+  },
+  {
+    id: "2",
+    title: "Discord",
+    icon: discord,
+    width: 36,
+    height: 28,
+  },
+  {
+    id: "3",
+    title: "Slack",
+    icon: slack,
+    width: 34,
+    height: 35,
+  },
+  {
+    id: "4",
+    title: "Photoshop",
+    icon: photoshop,
+    width: 34,
+    height: 34,
+  },
+  {
+    id: "5",
+    title: "Protopie",
+    icon: protopie,
+    width: 34,
+    height: 34,
+  },
+  {
+    id: "6",
+    title: "Framer",
+    icon: framer,
+    width: 26,
+    height: 34,
+  },
+  {
+    id: "7",
+    title: "Raindrop",
+    icon: raindrop,
+    width: 38,
+    height: 32,
+  },
+
+];
+
+export const pricing = [
+  {
+    id: "0",
+    title: "Beginner's Guide to Bitcoin",
+    description: "Basic Wallet Setup, foundational trading concepts, general trading accoun",
+    price: "0",
+    features: [
+      "Understand Bitcoin's history and key concepts",
+      "Learn how to buy, store, and use Bitcoin safely",
+      "Build a solid foundation for your Bitcoin journey",
+    ],
+  },
+  {
+    id: "1",
+    title: "Advanced Bitcoin",
+    description: "Advanced Wallet Management, sophisticated trading strategies, premium trading account",
+    price: "9.99",
+    features: [
+      "Explore Bitcoin's underlying technology and principles",
+      "Study advanced topics like smart contracts and the Lightning Network",
+      "Learn enhanced security practices for experienced users",
+    ],
+  },
+  {
+    id: "2",
+    title: "Bitcoin for Businesses",
+    description: "Wallet Address, advanced analytics, dedicated account",
+    price: null,
+    features: [
+      "Learn to set up Bitcoin payment systems for your business",
+      "Understand how to manage Bitcoin transactions efficiently",
+      "Explore regulatory considerations and benefits for businesses",
+    ],
+  },
+];
+
+export const benefits = [
+  {
+    id: "0",
+    title: "Ask admins anything",
+    text: "Got questions about Bitcoin? Our knowledgeable community is here to help, ready to share insights and answer any questions you might have.",
+    backgroundUrl: "./src/assets/benefits/card-1.svg",
+    iconUrl: benefitIcon1,
+    imageUrl: benefitImage2,
+  },
+  {
+    id: "1",
+    title: "Improve everyday",
+    text: "Enhance your Bitcoin knowledge daily with our resources and workshops. Stay updated with the latest trends and advancements in cryptocurrency.",
+    backgroundUrl: "./src/assets/benefits/card-2.svg",
+    iconUrl: benefitIcon2,
+    imageUrl: benefitImage2,
+    light: true,
+  },
+  {
+    id: "2",
+    title: "Connect everywhere",
+    text: "Join a global network of Bitcoin enthusiasts. Connect with like-minded individuals worldwide, fostering collaboration and knowledge sharing.",
+    backgroundUrl: "./src/assets/benefits/card-3.svg",
+    iconUrl: benefitIcon3,
+    imageUrl: benefitImage2,
+  },
+  {
+    id: "3",
+    title: "Fast responding",
+    text: "Our dedicated support team is always ready to assist you. We pride ourselves on fast response times, ensuring prompt information and support.",
+    backgroundUrl: "./src/assets/benefits/card-4.svg",
+    iconUrl: benefitIcon4,
+    imageUrl: benefitImage2,
+    light: true,
+  },
+  {
+    id: "4",
+    title: "Join Events",
+    text: "Got questions about Bitcoin? Our knowledgeable community is here to help, ready to share insights and answer any questions you might have.",
+    backgroundUrl: "./src/assets/benefits/card-5.svg",
+    iconUrl: benefitIcon1,
+    imageUrl: benefitImage2,
+  },
+  {
+    id: "5",
+    title: "Improve everyday",
+    text: "The app uses natural language processing to understand user queries and provide accurate and relevant responses.",
+    backgroundUrl: "./src/assets/benefits/card-6.svg",
+    iconUrl: benefitIcon2,
+    imageUrl: benefitImage2,
+  },
+];
+
+export const socials = [
+  {
+    id: "0",
+    title: "Discord",
+    iconUrl: discordBlack,
+    url: "https://discord.gg/4rbPVACK",
+  },
+  {
+    id: "1",
+    title: "Twitter",
+    iconUrl: twitter,
+    url: "https://x.com/btcnairobi",
+  },
+  {
+    id: "2",
+    title: "Instagram",
+    iconUrl: instagram,
+    url: "www.instagram.com/btcnairobi",
+  },
+  {
+    id: "3",
+    title: "Telegram",
+    iconUrl: telegram,
+    url: "t.me/btcnairobi",
+  },
+  {
+    id: "4",
+    title: "Facebook",
+    iconUrl: facebook,
+    url: "www.facebook.com/groups/btcnairobi",
+  },
+  {
+    id: "4",
+    title: "Linkedin",
+    iconUrl: linkedin,
+    url: "https://www.linkedin.com/company/btcnairobi/",
+  },
+];
